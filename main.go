25cd8c3dc8699ec00b966a2ff57fa83df1b28214
dylansong <dylan.song@gmail.com>
2021-05-27 21:13:03 +0000

    0528

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Name string `json:"name" form:"name" bind:"required"`
+	Age  int    `json:"age" form:"age" bind:"required,min=6,max=100"`
+}
+
 func main() {
 	fmt.Println("hello world!!")
 	r := gin.Default()
-	r.GET("/",func(c *gin.Context){
-		c.String(http.StatusOK,"good")
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"name": "xiaosong"})
+	})
+	r.POST("/user", func(c *gin.Context) {
+		var user User
+		err := c.BindJSON(user)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		c.JSON(http.StatusOK, gin.H{"message": user.Name, "age": user.Age})
 	})
 	r.Run()
 }
